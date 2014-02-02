06ccc6b9f9598d036663bffbae5e98cda125c651
patrickheeney <patrickheeney@gmail.com>
2014-02-01 18:45:24 -0700

    Add new protobox dashboard

--- README.md
+++ README.md
@@ -13,15 +13,11 @@ Alternatively, you can install it via git manually.
     git clone git@github.com:protobox/protobox.git protobox
     cd protobox && cp data/config/common.yml-dist data/config/common.yml
 
-Make sure you add an entry to your `/etc/hosts` for any virtualhosts in your `data/config/common.yml` file:
-
-	192.168.5.10    protobox.dev www.protobox.dev
-
-Then run `vagrant up` and pull up `http://protobox.dev` in your browser to see if it is setup correctly.
+Then run `vagrant up` and pull up `http://192.168.5.10/` in your browser to see if it is setup correctly.
 
 ## Configuration ##
 
-The protobox configuration file is found at `data/config/common.yml`. You can easily install new services by setting `install: 1` under any of the software in the configuration file. 
+The protobox configuration file is found at `data/config/common.yml`. You can easily install new services by setting `install: 1` under any of the software in the configuration file. Alternatively you can drag and drop your yml file to make changes with our web gui at [getprotobox.com](http://getprotobox.com). 
 
 ## Functionality ##
 
--- ansible/shell/finish-setup.rb
+++ ansible/shell/finish-setup.rb
@@ -1,7 +1,9 @@
 #!/usr/bin/env ruby
 
+require 'rubygems'
 require 'optparse'
 require 'yaml'
+require 'json'
 
 options = {}
 OptionParser.new do |opts|
@@ -39,19 +41,81 @@ File.open(logo_file, 'r') do |f1|
   end  
 end
 
-puts "\nYou can now access your websites at:\n"
-
 data = YAML::load_file(yaml_file);
+
+if !data['vagrant']['vm']['network']['private_network'].nil?
+  ip = data['vagrant']['vm']['network']['private_network']
+else
+  ip = nil
+end
+
+if !ip.nil?
+  puts "\nDashboard: http://" + ip + "/\n"
+end
+
+#
+# Build json
+#
+
+# Websites
+websites = []
 
 if !data['apache'].nil? and data['apache']['install'] == 1
   data['apache']['vhosts'].each do |vhost|
-    puts "http://" + vhost['servername'] + "\n"
+    websites.push({
+      :name => vhost['name'], 
+      :site => "http://" + vhost['servername'] + (vhost['port'].to_i != 80 ? ':' + vhost['port'] : '')
+    })
   end
 end
 
 if !data['nginx'].nil? and data['nginx']['install'] == 1
   data['nginx']['vhosts'].each do |vhost|
-    puts "http://" + vhost['server_name'] + "\n"
+    websites.push({
+      :name => vhost['name'], 
+      :site => "http://" + vhost['server_name'] + (vhost['port'].to_i != 80 ? ':' + vhost['port'] : '')
+    })
   end
 end
 
+# Databases
+databases = []
+
+if !data['mysql'].nil? and data['mysql']['install'] == 1
+  data['mysql']['databases'].each do |vhost|
+    databases.push({
+      :name => vhost['name'], 
+      :type => "mysql"
+    })
+  end
+end
+
+if !data['mariadb'].nil? and data['mariadb']['install'] == 1
+  data['mariadb']['databases'].each do |vhost|
+    databases.push({
+      :name => vhost['name'], 
+      :type => "mariadb"
+    })
+  end
+end
+
+if !data['postgresql'].nil? and data['postgresql']['install'] == 1
+  data['postgresql']['databases'].each do |vhost|
+    databases.push({
+      :name => vhost['name'], 
+      :type => "postgresql"
+    })
+  end
+end
+
+# Build document
+document = {
+  "version" => data['protobox']['version'],
+  "sites" => websites,
+  "databases" => databases
+}
+
+File.open("/srv/www/web/protobox/data.json", "w") do |f|
+  #f.write(document.to_json)
+  f.write(JSON.pretty_generate(document))
+end
