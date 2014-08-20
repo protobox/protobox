3a2de051a318da315c1b21c065b3bafab759f523
patrickheeney <patrickheeney@gmail.com>
2014-08-20 14:40:34 -0700

    Move ansible version back to latest since release of 1.7

--- lib/shell/build-protobox.rb
+++ lib/shell/build-protobox.rb
@@ -2,10 +2,10 @@ def build_protobox(yaml, protobox_dir)
   ansible_version_file = protobox_dir + '/ansible_version'
 
   # Ansible version
-  ansible_version = '1.6.8'
-  #if !yaml['protobox'].nil? and !yaml['protobox']['ansible'].nil? and !yaml['protobox']['ansible']['version'].nil?
-  #  ansible_version = yaml['protobox']['ansible']['version'].to_s
-  #end
+  ansible_version = 'latest'
+  if !yaml['protobox'].nil? and !yaml['protobox']['ansible'].nil? and !yaml['protobox']['ansible']['version'].nil?
+    ansible_version = yaml['protobox']['ansible']['version'].to_s
+  end
 
   # Dump out the contents
   File.open(ansible_version_file, "w") do |f|
